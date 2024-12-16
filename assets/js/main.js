9cff075b794c06bbc9254e52a16c0cc64f552bb0
Igor Dubik <whiteriver.devprojects@gmail.com>
2024-12-16 16:32:03 +1100

    Finished JS for both mobile and desktop

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,17 +3,48 @@ const userContainer = document.querySelector('.user');
 const shareContainer = document.querySelector('.share-container');
 const footer = document.querySelector('.footer');
 
-shareButton.addEventListener('click', () => {
-    if(userContainer.style.display !== 'none') {
-        userContainer.style.display = 'none';
-        shareContainer.style.display = 'flex';
-        footer.classList.add('dark-grey-background');
-        shareButton.classList.add('active');
+
+
+// This logic checks screen size then runs mobile/desktop logic based on screen size
+shareButton.addEventListener('click', () => { 
+    if (window.innerWidth < 768) {
+            if(userContainer.style.display !== 'none') {
+                userContainer.style.display = 'none';
+                shareContainer.style.display = 'flex';
+                footer.classList.add('dark-grey-background');
+                shareButton.classList.add('active');
+            }
+            else {
+                shareContainer.style.display = 'none';
+                footer.classList.remove('dark-grey-background');
+                shareButton.classList.remove('active');
+                userContainer.style.display = 'flex';
+            }
     }
     else {
-        shareContainer.style.display = 'none';
-        footer.classList.remove('dark-grey-background');
-        shareButton.classList.remove('active');
-        userContainer.style.display = 'flex';
+
+            if(shareContainer.style.display !== 'none') {
+                shareContainer.style.display = 'none';
+                shareButton.classList.remove('active');
+            }
+            else {
+                shareContainer.style.display = 'flex';
+                shareButton.classList.add('active');
+            }
     }
 })
+
+function checkScreenSize() {
+    if (window.innerWidth < 768 && shareContainer.style.display !== 'none') {
+        shareContainer.style.display = 'none';
+        shareButton.classList.remove('active');
+    }
+}
+
+window.addEventListener('resize', checkScreenSize)
+
+
+
+
+
+
